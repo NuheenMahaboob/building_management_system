ad22818cb59675dd93e2063011f88a9f03a572b1
Nuheen <nuheen.mahaboob@gmail.com>
2026-04-27 23:53:01 +0600

    Resident system updates

--- building_management_system/core/urls.py
+++ building_management_system/core/urls.py
@@ -15,4 +15,14 @@ urlpatterns = [
     path('pending_complaints/', views.PendingComplaint_view, name='pending_complaints'),
     path('solved_complaints/', views.SolvedComplaint_view, name='solved_complaints'),
     path('notices/', views.notices_view, name='notices'),
-    path('reports/', lambda request: views.manager_report_view(request, 1), name='reports')]
+    path('reports/', lambda request: views.manager_report_view(request, 1), name='reports'),
+    # ei 3tar jekono akta dilei login page e jabe
+    path('', views.login_view, name='login_root'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('resident-dashboard/', views.resident_dashboard, name='resident_dashboard'),
+    path('profile/', views.profile_view, name='profile'),
+    path('resident_notices/', views.resident_notices, name='resident_notices'),
+    path('resident_billings/', views.resident_billings, name='resident_billings'),
+    path('pay/<int:bill_id>/', views.pay_bill, name='pay_bill'),
+    path('resident_complaints/', views.resident_complaints, name='resident_complaints'),]
